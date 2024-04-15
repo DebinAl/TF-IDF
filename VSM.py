33b0381002e524d78a059cf7c7497beb9d213604
NgurahDhananjaya <99886528+NgurahDhananjaya@users.noreply.github.com>
2024-04-16 03:19:08 +0800

    Update(VSM): done

--- VSM.py
+++ VSM.py
@@ -3,6 +3,7 @@ import math
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import numpy as np
 
 def pre_procesing(document, language_option = None):
     porter_stemmer = PorterStemmer()
@@ -12,7 +13,6 @@ def pre_procesing(document, language_option = None):
     document = [sentence for sentence in document.split('.')] #split text
 
     for index, sentence in enumerate(document):
-        print(sentence)
         arr_sentence = []
         sentence = str(sentence).translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation))) #punctuation
         for word in sentence.split():                                                                        #tokenisasi
@@ -25,6 +25,14 @@ def pre_procesing(document, language_option = None):
         document[index] = arr_sentence
     
     return document[:-1]
+
+def term_list(pre_procesed):
+    word_li = []
+    for term in pre_procesed:
+        for word in term:
+            if word not in word_li:
+                word_li.append(word)
+    return word_li
         
 def tf(document, query_list):
     tf_in_doc = []
@@ -55,36 +63,109 @@ def idf(ddf):
         inverse_doc_freq.append(math.log10(val))
     return inverse_doc_freq
 
-def query_distance():
-    print("untuk mencari bobot jarak query")
 
-def document_distance():
-    print("untuk mencari bobot jarak document")
+def query_weight(word_list, find_query, idf, tf):
+    query_weight = []
+    for word, idf_val in zip(word_list, idf):
+        if word in find_query :
+            val = 1*idf_val
+            query_weight.append(val)
+        else:
+            val = 0*idf_val
+            query_weight.append(val)
     
-def dot_product():
-    print("mencari dot product")
-
-def cosinus_similarity():
-    print("Nilai Cosinus sudut antara vector kata kunci dengan tiap dokumen")
-
-def similarity_rank():
-    print("mengurutkan menurut tingkat kesamaan")
+    for tf_val in tf:
+        print("dawdaw")
+            
     
+    return query_weight
+
+def doc_weight(tf, idf):
+    weight_by_Term = []
+    for term_freq, doc_freq in zip(tf, idf):
+        term_row = []
+        for term_val in term_freq:
+            term_row.append(term_val*doc_freq)
+        weight_by_Term.append(term_row)
+    return weight_by_Term
+    
+def query_distance(query_weight):
+    square_val = []
+    for weight in query_weight:
+        square_val.append(weight**2) 
+    return math.sqrt(sum(square_val))
+  
+    
+def document_distance(doc_weight):
+    square_val = [[] for _ in range(len(doc_weight[0]))]
+    for weight_li in doc_weight:
+        for index, weight in enumerate(weight_li):
+            square_val[index].append(weight**2)
+            
+    doc_distance = []
+    for val in square_val:
+        doc_distance.append(math.sqrt(sum(val)))
+
+    return doc_distance
+    
+    
+def dot_product(query_weight, doc_weight):
+    query_square_val = []
+    for weight in query_weight:
+        query_square_val.append(weight**2) 
+    
+    doc_square_val = [[] for _ in range(len(doc_weight[0]))]
+    for weight_li in doc_weight:
+        for index, weight in enumerate(weight_li):
+            doc_square_val[index].append(weight**2)
+    
+    dot_prod = []
+    for index, ds_val_li in enumerate(doc_square_val):
+        doc_prod = []
+        for qs_val, ds_val in zip(query_square_val, ds_val_li):
+            doc_prod.append(qs_val*ds_val)
+        dot_prod.append(sum(doc_prod))
+
+    return dot_prod
+
+def cosinus_similarity(dot_product, query_distance, doc_distance):
+    cosinus_sim = []
+    for dot_val, doc_dval in zip(dot_product, doc_distance):
+        cosinus_sim.append(dot_val/(query_distance*doc_dval))
+    return cosinus_sim
+    
+def rank_similarity(cosinus_similarity):
+    rank_dict = {}
+    for index, sim_val in enumerate(cosinus_similarity):
+        rank_dict[index] = sim_val
+    print(sorted(rank_dict.items(), key=lambda x: x[1], reverse=True))
+    
+#dataset (example)
 id_language = "indonesian"
-test_query = ['sandang','pangan','papan']
-id_language_test = 'Manusia memerlukan sandang. Manusia membutuhkan pangan pangan untuk hidup papan. Manusia memerlukan sandang untuk hidup, karena papan adalah rumah.'
-id_text = "Studi tentang penggunaan teknologi dalam pendidikan menunjukkan pertumbuhan pesat dalam pengembangan platform pembelajaran online. Penelitian ini menggunakan corpus yang terdiri dari data dari berbagai institusi pendidikan. Analisis terhadap korpus ini mengungkapkan pola-pola penggunaan teknologi yang beragam dan strategi pengajaran yang efektif. Dengan memanfaatkan corpus ini, pendidik dapat merancang pengalaman pembelajaran yang lebih adaptif dan terkini bagi para siswa."
+find_query = ['sistem']
+id_language_test = 'Sistem adalah kumpulan elemen. Adalah kumpulan elemen yang saling berinteraksi. Sistem berinteraksi untuk mencapai tujuan.'
 
-en_language = "english"
-en_text = "Python programmers often tend like programming in python because it's like english. We call people who program in python pythonistas. This research utilizes a corpus consisting of data from various educational institutions. Analysis of this corpus reveals diverse patterns of technology use and effective teaching strategies. By leveraging this corpus, educators can design more adaptive and up-to-date learning experiences for students."
+#pre processing
+pre_procesing_result = pre_procesing(id_language_test, id_language)#udah bener
+word_list_result = term_list(pre_procesing_result)#udah bener
 
+#TF-IDF
+tf_result = tf(pre_procesing_result, word_list_result)#udah bener
+df_result = df(tf_result)#udah bener
+ddf_result = ddf(pre_procesing_result, df_result)#udah bener
+idf_result = idf(ddf_result) #udah bener
+query_weight_result = query_weight(word_list_result, find_query, idf_result, tf_result) #udah bener
+doc_weight_result = doc_weight(tf_result, idf_result) #udah bener
 
-pre_procesing_result = pre_procesing(id_language_test, id_language)
-tf_result = tf(pre_procesing_result, test_query)
-df_result = df(tf_result)
-ddf_result = ddf(pre_procesing_result, df_result)
-idf_result = idf(ddf_result)
-doc_result = doc_weight(tf_result, idf_result)
-total_result =  total_weight(doc_result)
+#PERHITUNGAN JARAK
+query_distance_result = query_distance(query_weight_result)
+document_distance_result = document_distance(doc_weight_result)
 
-print(total_result)
+#DOT PRODUCT
+dot_product_result = dot_product(query_weight_result, doc_weight_result)
+
+#cosin similarity
+cosinus_similarity_result = cosinus_similarity(dot_product_result, query_distance_result, document_distance_result)
+
+#similarity_rank
+rank_similarity(cosinus_similarity_result)
